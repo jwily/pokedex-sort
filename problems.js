f2db4b0aea7929d982aaa251a68b1bf77cdc2f7d
jwily <johnlee1120@gmail.com>
2023-02-28 17:08:35 -0800

    solutions

--- problems.js
+++ problems.js
@@ -67,14 +67,37 @@ Sort by sum of all base stats descending
 
 const statSort = (arr) => {
 
-  // Code here
+  const compare = (a, b) => {
+
+    // a and b are Pokemon here
+
+    const statsA = Object.values(a.base);
+    const statsB = Object.values(b.base);
+
+    const sumA = statsA.reduce((sum, curr) => sum + curr, 0);
+    const sumB = statsB.reduce((sum, curr) => sum + curr, 0);
+
+    // -1, 0, 1?
+    // We have a and b
+    // The question is, where does "a" go in relation to "b"
+    // given any conditional?
+
+    // If we want "a" to come before "b", we want to return -1
+    // because -1 is "left", right?
+    // 1 is right
+
+    return sumB - sumA;
+
+  }
+
+  return arr.sort(compare);
 
 }
 
-// statSort(genOne);
-// console.log('\nSorted by sum of base stats\n');
-// console.log(genOne.map(poke =>
-//   `${poke.name.english}: ${Object.values(poke.base).join(', ')}`))
+statSort(genOne);
+console.log('\nSorted by sum of base stats\n');
+console.log(genOne.map(poke =>
+  `${poke.name.english}: ${Object.values(poke.base).join(', ')}`))
 
 /*
 
@@ -84,16 +107,26 @@ Sort by alphabetically by French name
 
 const frenchSort = (arr) => {
 
-  // Code here
+  const compare = (a, b) => {
 
+    const nameA = a.name.french;
+    const nameB = b.name.french;
+
+    if (nameA < nameB) return -1;
+    else if (nameA > nameB) return 1;
+    return 0;
+
+  }
+
+  return arr.sort(compare);
 }
 
-// // Reset the original array
-// genOne = pokedex.slice(0, 151);
+// Reset the original array
+genOne = pokedex.slice(0, 151);
 
-// frenchSort(genOne);
-// console.log('\nSorted by French name\n');
-// console.log(genOne.map(poke => poke.name.french));
+frenchSort(genOne);
+console.log('\nSorted by French name\n');
+console.log(genOne.map(poke => poke.name.french));
 
 /*
 
@@ -103,13 +136,34 @@ Sort by type; if secondary type exists, sort single-types first then by secondar
 
 const typeSort = (arr) => {
 
-  // Code here
+  const compare = (a, b) => {
 
+    const typesA = a.type;
+    const typesB = b.type;
+
+    if (typesA[0] === typesB[0]) {
+
+      const secondA = typesA[1] ? typesA[1] : '';
+      const secondB = typesB[1] ? typesB[1] : '';
+
+      if (secondA < secondB) return -1;
+      if (secondA > secondB) return 1;
+      return 0;
+
+    }
+
+    if (typesA[0] < typesB[0]) return -1;
+    else if (typesA[0] > typesB[0]) return 1;
+    return 0;
+
+  }
+
+  return arr.sort(compare);
 }
 
-// // Reset the original array
-// genOne = pokedex.slice(0, 151);
+// Reset the original array
+genOne = pokedex.slice(0, 151);
 
-// typeSort(genOne);
-// console.log('\nSorted by type\n');
-// console.log(genOne.map(poke => `${poke.name.english}: ${poke.type.join(' / ')}`));
+typeSort(genOne);
+console.log('\nSorted by type\n');
+console.log(genOne.map(poke => `${poke.name.english}: ${poke.type.join(' / ')}`));
